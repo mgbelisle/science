8a08905991426ce44ca7eee65e507af67dae2565
Matthew Belisle <matthew.belisle@workiva.com>
2024-07-19 12:37:47 -0500

    WIP

--- cryptopals/1/6/main.kt
+++ cryptopals/1/6/main.kt
@@ -1,3 +1,4 @@
+import java.io.FileInputStream
 import java.io.FileReader
 import java.util.Base64
 import kotlin.ByteArray
@@ -15,6 +16,60 @@ private fun hammingDistance(a: ByteArray, b: ByteArray): Int {
     return d
 }
 
+private fun hexToBytes(hexString: String): ByteArray {
+    return hexString.chunked(2).map { it.toInt(16).toByte() }.toByteArray()
+}
+
+private fun xor(a: ByteArray, b: Byte): ByteArray {
+    val c = ByteArray(a.size)
+    a.forEachIndexed { i, a2 -> c[i] = (a2.toInt() xor b.toInt()).toByte() }
+    return c
+}
+
+// Given a bytearray, return a map {byte: count}
+private fun byteArrayToCharCountMap(byteArray: ByteArray): Map<Byte, Int> {
+    return byteArray
+            .map { it.toInt().toChar().lowercaseChar().code.toByte() } // Case doesn't matter
+            .groupingBy { it }
+            .eachCount()
+}
+
+// LOTR char count map
+private val englishCharCountMap =
+        FileInputStream("lotr.txt").use { byteArrayToCharCountMap(it.readBytes()) }
+private val englishCharCountSum = englishCharCountMap.values.sum()
+
+// Given a bytearray, return the amount that it differs from the english char count map
+private fun englishDiff(byteArray: ByteArray): Double {
+    val map = byteArrayToCharCountMap(byteArray)
+    val sum = map.values.sum()
+    return map
+            .map { (letter, count) ->
+                Math.abs(
+                        (count.toDouble() / sum) -
+                                (englishCharCountMap[letter] ?: 0).toDouble() / englishCharCountSum
+                )
+            }
+            .sum()
+}
+
+private fun singleKeyXor(byteArray: ByteArray): ByteArray {
+    var plaintext = ByteArray(0)
+    var diff = Double.MAX_VALUE
+    for (line in FileInputStream("1/4/4.txt").bufferedReader().lineSequence()) {
+        val ciphertext = hexToBytes(line)
+        for (key in Byte.MIN_VALUE..Byte.MAX_VALUE) {
+            val plaintext2 = xor(ciphertext, key.toByte())
+            val diff2 = englishDiff(plaintext2)
+            if (diff2 < diff) {
+                diff = diff2
+                plaintext = plaintext2
+            }
+        }
+    }
+    return plaintext
+}
+
 fun main() {
     // Assert that the hamming distance between two strings is as expected
     if (hammingDistance("this is a test".toByteArray(), "wokka wokka!!!".toByteArray()) != 37) {
@@ -27,7 +82,7 @@ fun main() {
                 it.flatMap { Base64.getDecoder().decode(it).asIterable() }.toList().toByteArray()
             }
 
-    for ((keySize, dist) in
+    for ((keySize, _) in
             (2..40).asSequence()
                     .map { keySize ->
                         keySize to
@@ -37,7 +92,7 @@ fun main() {
                                 ) / keySize.toDouble()
                     }
                     .sortedBy { it.second }) {
-        // break the ciphertext into blocks of KEYSIZE length
         val blocks = ciphertext.asList().chunked(keySize).map { it.toByteArray() }
+        val transposed = (1..keySize).map { i -> blocks.map { it[i] }.toByteArray() }
     }
 }
